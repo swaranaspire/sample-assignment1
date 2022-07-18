b41868f1865e83c40ab5df28b9fb35621dd2cb79
swaranaspire <swarankumar.akula@aspiresys.com>
2022-07-18 19:58:56 +0530

    created registration form with validators

--- sampleassignment/src/app/registrationform/registrationform.component.ts
+++ sampleassignment/src/app/registrationform/registrationform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrationform',
@@ -6,7 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrationform.component.css']
 })
 export class RegistrationformComponent implements OnInit {
+public registrationForm: FormGroup = new FormGroup({
+  firstname: new FormControl(null, [Validators.required,]),
+  lastname: new FormControl(null, [Validators.required,]),
+  // gender: new FormControl(null, [Validators.required,]),
+  birth:new FormControl(null, [Validators.required,]),
+  // country:new FormControl(null,[Validators.required,]),
+  file:new FormControl(null,[Validators.required,])
 
+
+
+
+
+
+});
+register(){
+  this.registrationForm.markAllAsTouched();
+  console.log(this.registrationForm.value);
+  
+}
   constructor() { }
 
   ngOnInit(): void {
